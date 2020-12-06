3e1b0baeab11e192900fab679fa5065236a55bbd
Wiktor Jóźwik <w.jozwik@stud.elka.pw.edu.pl>
2020-12-07 00:45:19 +0100

    Tic Tac Toe upload

---
+++ js/AI.js
@@ -0,0 +1,67 @@
+function alphaBeta(board, alpha, beta, isMax){
+    const winner = checkIfEndOfGame(skipMessage = true)
+    if(winner != null){
+        return score.get(winner);
+    }
+
+    if(isMax){
+        let maxScore = -100;
+        for(let i=0; i<9; i++){
+            if(board[i] === ''){
+                board[i] = player;
+                isMax = false;
+                let score = alphaBeta(board, alpha, beta, isMax);
+                board[i] = '';
+                maxScore = Math.max(score, maxScore);
+                alpha = Math.max(alpha, maxScore);
+                if(beta <= alpha){
+                    break;
+                }
+        }
+    }
+    return maxScore
+    } else {
+        let minScore = 100;
+            for(let i=0; i<9; i++){
+                if(board[i] === ''){
+                    board[i] = AI;
+                    isMax = true;
+                    let score = alphaBeta(board, alpha, beta, isMax);
+                    board[i] = '';
+                    minScore = Math.min(score, minScore);
+                    alpha = Math.min(alpha, minScore);
+                    if(beta <= alpha){
+                        break;
+                    }
+            }
+        }
+        return minScore
+}
+}
+
+function AImove() {
+    let score, bestMove
+    let alpha = -100;
+    let beta = 100
+    let bestScore = 100;
+    let isMax = true;
+    for(let i=0; i<9; i++){
+        if(board[i] === ''){
+            board[i] = AI;
+            score = alphaBeta(board, alpha, beta, isMax)
+            board[i] = '';
+            if(score<bestScore){
+                bestScore = score;
+                bestMove = i; 
+            }
+        }
+    }
+    board[bestMove] = AI;
+    document.querySelectorAll('.box').forEach(e => {
+        const index = parseInt(e.getAttribute('index'));
+        if(index === bestMove){
+            e.innerHTML = AI;
+        }
+    });
+
+}
---
+++ js/script.js
@@ -0,0 +1,117 @@
+function equals3(a, b, c) {
+    return a === b && b === c && a === c && a !== '';
+}
+
+function checkIfEndOfGame(skipMessage) {
+    let winner = null;
+    const winCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < 8; i++) {
+        const winCombination = winCombinations[i];
+        if (
+            equals3(
+                board[winCombination[0]],
+                board[winCombination[1]],
+                board[winCombination[2]]
+            )
+        ) {
+            winner = board[winCombination[0]];
+            const msg = winner === player ? winMessage : loseMessage;
+            if (!skipMessage) {
+                printWinner(msg);
+                activeGame = false;
+            }
+            return winner;
+        }
+    }
+
+    if (!board.includes('')) {
+        winner = 'TIE';
+        if (!skipMessage) {
+            printWinner(tieMessage);
+            activeGame = false;
+        }
+        return winner;
+    }
+}
+
+function printWinner(msg) {
+    const status = document.querySelector('.status');
+    status.innerHTML = msg;
+}
+
+function restartGame() {
+    board = ['', '', '', '', '', '', '', '', ''];
+    document.querySelectorAll('.box').forEach((e) => {
+        e.innerHTML = '';
+    });
+    document.querySelector('.status').innerHTML = '';
+    activeGame = true;
+}
+
+function boxClick(box) {
+    const boxClicked = box.target;
+    const boxNum = parseInt(boxClicked.getAttribute('index'));
+    if (!activeGame) return;
+    if (board[boxNum] === '') {
+        if (currentPlayer === player) {
+            boxClicked.innerHTML = currentPlayer;
+            board[boxNum] = currentPlayer;
+
+            currentPlayer = AI;
+            checkIfEndOfGame((skipMessage = false));
+        }
+
+        if (currentPlayer === AI) {
+            AImove();
+            currentPlayer = player;
+            checkIfEndOfGame((skipMessage = false));
+        }
+    }
+}
+
+const score = new Map();
+score.set('X', 1);
+score.set('O', -1);
+score.set('TIE', 0);
+
+const winMessage = "Congratulations, you've won";
+const loseMessage = 'Unfortunately the AI is too strong :(';
+const tieMessage = "It's a draw, you are as good as the AI";
+
+const player = 'X';
+const AI = 'O';
+let currentPlayer = player;
+let board = ['', '', '', '', '', '', '', '', ''];
+let activeGame = true;
+
+const boxSelected = document.querySelectorAll('.box');
+
+let restart = document.querySelector('.restart').addEventListener('click', restartGame);
+
+let boxes = boxSelected.forEach(e => e.addEventListener('click', boxClick));
+
+console.log(activeGame);
+
+
+
+
+// boxSelected.forEach(box => box.addEventListener('mouseenter', e =>{
+//     if(e.target === ''){
+//         e.target.innerHTML = 'X'
+//     }
+
+// }));
+// boxSelected.forEach(box => box.addEventListener('mouseleave', e =>{
+//     if(e.target === ''){
+//         e.target.innerHTML = 'X'
+//     }
+// }));
